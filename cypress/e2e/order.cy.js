b2f52eda155edd34f94735bafc390442f1eb4efc
Bbondaruk <mega.bondaruk@gmail.com>
2023-07-16 18:20:31 +0300

    Added function for finding product by name - to helper file

--- cypress/e2e/order.cy.js
+++ cypress/e2e/order.cy.js
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker';
 import user from '../fixtures/user.json';
-import {loginViaUI} from '../support/helper';
+import {loginViaUI, findProductByName} from '../support/helper';
 
 it('Place order', () => {
 
@@ -32,23 +32,35 @@ it('Place order', () => {
     cy.get('.maintext').should('contain', 'Your Order Has Been Processed!');
 })
 
-it('Place order HW', () => {
+it.only('Place order HW', () => {
 
   loginViaUI(user);
   
-  cy.log('Add random product to cart from main page')
+  cy.log('Find and add product to cart from search page')
   cy.visit('/');
 
   cy.get('input#filter_keyword').type('i{enter}');
 
-  findProductByName('Acqua Di Gio Pour Homme');
+  let productName = 'Acqua Di Gio Pour Homme'
+  findProductByName(productName);
+  
+  cy.get('.contentpanel [class="thumbnails grid row list-inline"]').contains(productName)
+  .closest('[class="col-md-3 col-sm-6 col-xs-12"]').find('[class="fa fa-cart-plus fa-fw"]').click()
+  cy.log('before else')
+  if(productName == 'Creme Precieuse Nuit 50ml'){
+  cy.get('.contentpanel [class="thumbnails grid row list-inline"]').contains(productName)
+  .closest('[class="col-md-3 col-sm-6 col-xs-12"]').find('[class="fa fa-shopping-cart fa-fw"]').click()
+  } else{
+    cy.get('.productpagecart .cart').click()
+  }
+  cy.log('after else')
+  cy.log('Verify order')
+  cy.get('[class="container-fluid cart-info product-list"]')
+  .should('contain', productName)
 
-  //cy.get('.productcart').first().click();
-  //cy.get('.quick_basket').click();
-
-  cy.log('Open basket')
-  cy.get('#cart_checkout1').click();
-
+  cy.log('Checkout')
+  cy.get('[class="container-fluid cart-info product-list"] #cart_checkout1').click()
+  
   cy.log('Verify checkout data')
   cy.get('.table.confirm_shippment_options')
   .should('contain', user.firstName)
--- cypress/support/helper.js
+++ cypress/support/helper.js
@@ -18,3 +18,63 @@ export function login2(user){
     cy.get('#loginFrm button').click();
 
 }
+
+export function findProductByName(productName){
+    const visitNextPageIfPossible = () => {
+        cy.get('.contentpanel [class="thumbnails grid row list-inline"]').then((products) => {
+          if (products.find(`[title="${productName}"]`).length > 0) {
+            return
+          }
+          cy.get('.pagination').children().contains('>').click()
+          visitNextPageIfPossible()
+        })
+      }
+    
+      visitNextPageIfPossible()
+    
+      cy.log('знайшли продукт')
+    
+    //   return cy.get('запит діва з потрібним продуктом')
+
+
+ 
+//  cy.get('.contentpanel [class="thumbnails grid row list-inline"]')
+//  .then(products =>{
+//     let length =  products.find(`[title="${productName}"]`).length
+//     // if(length == 0) {
+//     //     cy.get('.pagination').children().contains('>').click()
+//     // } else{
+//     //     //продукт є - щось зробити
+//     // }
+    
+//     for(length; length < 1; length = products.find(`[title="${productName}"]`).length){
+        
+//         cy.get('.pagination').children().contains('>').click()
+        
+//     }
+//     cy.log('**CODE DOSHEL**')
+//  })
+//  for(let length = 0; length < 1;){
+//     cy.wait(5000)
+//     cy.get('.contentpanel [class="thumbnails grid row list-inline"]')
+//         .then(products => {
+//             length = products.find(`[title="${productName}"]`).length
+            
+//             cy.get('.pagination').children().contains('>').click()
+//         })
+//         length = 1
+//     cy.log('**CODE DOSHEL**')
+//  }
+    // cy.get('.contentpanel [class="thumbnails grid row list-inline"]')
+    // .then(products => {
+    //     // let length =  products.find(`[title="${productName}"]`).length
+    //     // if(length == 0){
+    //     //     cy.get('.pagination').children().contains('>').click()
+    //     // } else{
+    //     //     cy.log('Nashel')
+    //     // }
+    // })
+
+            
+          
+}
